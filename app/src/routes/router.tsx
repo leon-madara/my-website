b12b44f747d4e95056635c2f21078545cce424b2
Leon Madara <leon.madara@outlook.com>
2026-04-03 20:00:02 +0300

    feat: make contact rebuild the default contact route

--- app/src/routes/router.tsx
+++ app/src/routes/router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AppLayout } from "../shared/AppLayout";
 import { AboutRoute } from "../features/about/AboutRoute";
-import { ContactRoute } from "../features/contact/ContactRoute";
 import { ContactRebuildRoute } from "../features/contactRebuild/ContactRebuildRoute";
 import { DesignProcessRoute } from "../features/designProcess/DesignProcessRoute";
 import { HomeRoute } from "../features/home/HomeRoute";
@@ -26,7 +25,7 @@ export const router = createBrowserRouter([
       },
       {
         path: "contact",
-        element: <ContactRoute />
+        element: <ContactRebuildRoute />
       },
       {
         path: "contact-rebuild",
